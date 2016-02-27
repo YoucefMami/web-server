3461c660f86f5361444a2d29751c1f658535828a
Youcef Mami <youcef.mami@gmail.com>
2016-02-27 17:54:46 -0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+
+// variable in upper case ie it should be constant
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		
+		console.log('Request made on ' + new Date().toString() + ': ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+// needs to be first
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('This is the about page');
+});
+
+app.get('/help', function (req, res) {
+	res.send('This is the help');
+});
+
+// Folder we want to expose
+// Default file shown if none is specified in the browser
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT);
+});
